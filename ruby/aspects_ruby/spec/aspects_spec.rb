b7de30e061784a53fadd0701242ede980e7b874e
Tomas Ferraro <tetferraro@gmail.com>
2015-09-02 14:08:04 -0300

    Reorganizacion de las validaciones. Refactor

--- ruby/aspects_ruby/spec/aspects_spec.rb
+++ ruby/aspects_ruby/spec/aspects_spec.rb
@@ -24,5 +24,22 @@ describe 'Aspect tests' do
     expect { aspect.on(MiClase) }.to raise_error(ArgumentError, 'wrong number of arguments (0 for +1)')
   end
 
+  it 'falla por no pasarle orgien' do
+    aspect = Aspects.new
+    expect { aspect.on { 'hola' } }.to raise_error(ArgumentError, 'origen vacio')
+  end
+
+  it 'acepta regex de una clase que existe' do
+    class MiClase
+
+    end
+    aspect = Aspects.new
+    expect(aspect.on(/MiClase/) { 'hola' }).to eq ('Me pasaste (?-mix:MiClase) y hola')
+  end
+
+  it 'falla regex de una clase que no existe' do
+    aspect = Aspects.new
+    #expect{aspect.on(/Saraza/) { 'hola' }}.to raise_error(ArgumentError, 'origen vacio')
+  end
 
 end
--- ruby/aspects_ruby/src/aspects.rb
+++ ruby/aspects_ruby/src/aspects.rb
@@ -1,6 +1,37 @@
 class Aspects
   def on(*objetos, &condicion)
-    raise ArgumentError, 'wrong number of arguments (0 for +1)' if condicion.nil?
+    validar_argumentos(objetos, condicion)
     "Me pasaste #{objetos.join(', ')} y #{condicion.call}"
+  end
+
+  def validar_argumentos(objetos, condicion)
+    raise ArgumentError, 'wrong number of arguments (0 for +1)' if condicion.nil?
+    raise ArgumentError, 'origen vacio' if objetos.empty?
+    #objetos.select{ |origen| origen.is_a? Regexp}.matches()
+  end
+
+end
+
+class Class
+  def descendants
+    result = []
+    ObjectSpace.each_object(::Class) { |clase| result << clase if clase < self }
+    result
+  end
+end
+
+class Module
+  def descendants
+    result = []
+    ObjectSpace.each_object(::Module) { |clase| result << clase if clase < self }
+    result
+  end
+end
+
+class Object
+  def descendants
+    result = []
+    ObjectSpace.each_object(::Object) { |clase| result << clase if clase < self }
+    result
   end
 end
