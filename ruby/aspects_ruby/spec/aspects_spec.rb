edb293e0fca2a6e2f56c92cdb126144c52dcad77
Tomás Ferraro <thom_games@hotmail.com>
2015-09-02 17:21:08 -0300

    Agregado el parseo de multiples regex, failure ante ningun regex valido y se modifico Module para incluir un par de metodos utiles

--- ruby/aspects_ruby/spec/aspects_spec.rb
+++ ruby/aspects_ruby/spec/aspects_spec.rb
@@ -1,15 +1,19 @@
 require 'rspec'
 require_relative '../src/aspects'
 
-describe 'Aspect tests' do
+describe 'Aspect origenes' do
 
-  it 'chequear parametros pasados' do
+  before(:each) do
     class MiClase
 
     end
     module MiModulo
 
     end
+  end
+
+  it 'chequear parametros pasados' do
+
     miObjeto = MiClase.new
 
     aspect = Aspects.new
@@ -17,9 +21,6 @@ describe 'Aspect tests' do
   end
 
   it 'falla por no pasarle un bloque' do
-    class MiClase
-
-    end
     aspect = Aspects.new
     expect { aspect.on(MiClase) }.to raise_error(ArgumentError, 'wrong number of arguments (0 for +1)')
   end
@@ -30,16 +31,64 @@ describe 'Aspect tests' do
   end
 
   it 'acepta regex de una clase que existe' do
-    class MiClase
-
-    end
     aspect = Aspects.new
-    expect(aspect.on(/MiClase/) { 'hola' }).to eq ('Me pasaste (?-mix:MiClase) y hola')
+    expect(aspect.on(/MiClase/) { 'hola' }).to eq ('Me pasaste MiClase y hola')
   end
 
   it 'falla regex de una clase que no existe' do
     aspect = Aspects.new
-    #expect{aspect.on(/Saraza/) { 'hola' }}.to raise_error(ArgumentError, 'origen vacio')
+    expect { aspect.on(/Saraza/) { 'hola' } }.to raise_error(ArgumentError, 'origen vacio')
   end
 
+  it 'acepta regex parcial de una clase que existe' do
+    aspect = Aspects.new
+    expect(aspect.on(/^Mi.*/) { 'hola' }).to eq ('Me pasaste MiClase, MiModulo y hola')
+  end
+end
+
+
+describe 'Aspect parseo de regex y demaces' do
+
+  it 'obtener origen de varias regex' do
+    expect(Module.get_origin_by_multiple_regex([/Class/, /Object/])).to eq([Class, NilClass, TrueClass, FalseClass, Object, BasicObject, ObjectSpace])
+  end
+  it 'obtener nada con regex que no matchea' do
+    expect(Module.get_origin_by_multiple_regex([/CACA/])).to eq([])
+  end
+
+  it 'obtener origen de varias regex repetidas' do
+    expect(Module.get_origin_by_multiple_regex([/Class/, /Class/])).to eq([Class, NilClass, TrueClass, FalseClass])
+  end
+
+  it 'obtener origen de una regex' do
+    expect(Module.get_origin_by_regex(/Class/)).to eq([Class, NilClass, TrueClass, FalseClass])
+  end
+
+  it 'obtener origen de una regex' do
+    expect(Module.get_origin_by_regex(/Class/)).to eq([Class, NilClass, TrueClass, FalseClass])
+  end
+
+  it 'obtener simbolos de una regex' do
+    expect(Module._get_symbol_by_regex(/Class/)).to eq([:Class, :NilClass, :TrueClass, :FalseClass])
+  end
+
+  it 'obtener ningun simbolo porque no matcheo la regex' do
+    expect(Module._get_symbol_by_regex(/Saraza/)).to eq([])
+  end
+
+  it 'matcheo con nombres de clases' do
+    class MiClase
+
+    end
+    expect(Aspects.new.class_exists?('MiClase')).to eq(true)
+    expect(Aspects.new.class_exists?('MiClase2')).to eq(false)
+  end
+
+  it 'matcheo con nombres de modulos' do
+    module MiModulo
+
+    end
+    expect(Aspects.new.module_exists?('MiModulo')).to eq(true)
+    expect(Aspects.new.module_exists?('MiModulo2')).to eq(false)
+  end
 end
--- ruby/aspects_ruby/src/aspects.rb
+++ ruby/aspects_ruby/src/aspects.rb
@@ -1,37 +1,53 @@
 class Aspects
   def on(*objetos, &condicion)
-    validar_argumentos(objetos, condicion)
-    "Me pasaste #{objetos.join(', ')} y #{condicion.call}"
+    origenes = validar_argumentos(objetos, condicion)
+    "Me pasaste #{origenes.join(', ')} y #{condicion.call}"
   end
 
   def validar_argumentos(objetos, condicion)
     raise ArgumentError, 'wrong number of arguments (0 for +1)' if condicion.nil?
     raise ArgumentError, 'origen vacio' if objetos.empty?
-    #objetos.select{ |origen| origen.is_a? Regexp}.matches()
+    convertir_a_origenes_validos(objetos)
   end
 
-end
-
-class Class
-  def descendants
-    result = []
-    ObjectSpace.each_object(::Class) { |clase| result << clase if clase < self }
-    result
+  def convertir_a_origenes_validos(objetos)
+    regexs = objetos.select { |origen| origen.is_a? Regexp }
+    origenes_regex = Module.get_origin_by_multiple_regex(regexs)
+    raise ArgumentError, 'origen vacio' if !regexs.empty? && origenes_regex.empty?
+    objetos - regexs + origenes_regex
   end
+
+  def origen_exists?(regex)
+    Module.constants.any? do |constante|
+      !regex.match(constante).nil?
+    end
+  end
+
+
+  #Prueba de define_method, no tiene importancia
+  [Class, Module, Object].each do |origen|
+    define_method("#{origen}_exists?".downcase) do |nombre|
+      begin
+        Module.const_get(nombre).is_a?(origen)
+      rescue NameError
+        false
+      end
+    end
+  end
+
 end
 
 class Module
-  def descendants
-    result = []
-    ObjectSpace.each_object(::Module) { |clase| result << clase if clase < self }
-    result
+  def _get_symbol_by_regex(regex)
+    self.constants.select { |c| regex.match(c) }
   end
-end
 
-class Object
-  def descendants
-    result = []
-    ObjectSpace.each_object(::Object) { |clase| result << clase if clase < self }
-    result
+
+  def get_origin_by_regex(regex)
+    self._get_symbol_by_regex(regex).map { |symbol| self.const_get(symbol) }
+  end
+
+  def get_origin_by_multiple_regex(regex)
+    regex.map { |r| Module.get_origin_by_regex(r) }.flatten(1).uniq
   end
 end
