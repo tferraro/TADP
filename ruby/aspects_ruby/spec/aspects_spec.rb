4ca24cf2a66aae79b4e9b9481c1cb24005fdb55d
Tomas Ferraro <tetferraro@gmail.com>
2015-09-05 18:31:27 -0300

    Pulida la solucion, ya filtra correctamente con name, is_public y is_private

--- ruby/aspects_ruby/spec/aspects_spec.rb
+++ ruby/aspects_ruby/spec/aspects_spec.rb
@@ -3,6 +3,15 @@ require_relative '../src/aspects'
 
 describe 'Aspect condiciones' do
 
+  before(:all) do
+    class MiClase
+
+    end
+    module MiModulo
+
+    end
+  end
+
   it 'probar condiciones de visibilidad is_public y name' do
     class MiClase
       def self.bar
@@ -16,18 +25,17 @@ describe 'Aspect condiciones' do
       end
     end
     expect(
-        Aspects.on(MiClase, MiModulo) do |hey|
-          where name(/bar/), is_public, hey
-
+        Aspects.on(MiClase, MiModulo) do
+          where name(/bar/), is_public
         end).to eq('Me pasaste MiClase, MiModulo y [:bar, :bario]')
     expect(
-        Aspects.on(/^Mi.*/) do |hey|
-          where name(/bar/), is_public, hey
+        Aspects.on(/^Mi.*/) do
+          where name(/bar/), is_public
 
         end).to eq('Me pasaste MiClase, MiModulo y [:bar, :bario]')
   end
 
-  it 'prueba' do
+  it 'probamos el filtro de privados' do
     class MiClase
       def bar
       end
@@ -37,17 +45,17 @@ describe 'Aspect condiciones' do
     end
     mi_objetito = MiClase.new
     expect(
-        Aspects.on(mi_objetito) do |hey|
-          where name(/foo/), is_private, hey
+        Aspects.on(mi_objetito) do
+          where name(/foo/), is_private
 
         end).to eq("Me pasaste #{mi_objetito} y [:foo]")
   end
-  it ' pruba2' do
+  it 'probar condiciones de visibilidad is_public sin name' do
     expect(
-        Aspects.on(MiClase) do |hey|
-          where is_public, hey
+        Aspects.on(MiClase) do
+          where is_public
 
-        end).to eq('Me pasaste MiClase y [:bar, :foo, :allocate, :new, :superclass, :any_instance, :freeze, :===, :==, :<=>, :<, :<=, :>, :>=, :to_s, :inspect, :included_modules, :include?, :name, :ancestors, :instance_methods, :public_instance_methods, :protected_instance_methods, :private_instance_methods, :constants, :const_get, :const_set, :const_defined?, :const_missing, :class_variables, :remove_class_variable, :class_variable_get, :class_variable_set, :class_variable_defined?, :public_constant, :private_constant, :singleton_class?, :include, :prepend, :module_exec, :class_exec, :module_eval, :class_eval, :method_defined?, :public_method_defined?, :private_method_defined?, :protected_method_defined?, :public_class_method, :private_class_method, :autoload, :autoload?, :instance_method, :public_instance_method, :example_group, :describe, :context, :xdescribe, :xcontext, :fdescribe, :fcontext, :shared_examples, :shared_context, :shared_examples_for, :_get_symbol_by_regex, :get_origin_by_regex, :get_origin_by_multiple_regex, :where, :is_private, :is_public, :nil?, :=~, :!~, :eql?, :hash, :class, :singleton_class, :clone, :dup, :taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :frozen?, :methods, :singleton_methods, :protected_methods, :private_methods, :public_methods, :instance_variables, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, :remove_instance_variable, :instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?, :extend, :display, :method, :public_method, :singleton_method, :define_singleton_method, :object_id, :to_enum, :enum_for, :gem, :equal?, :!, :!=, :instance_eval, :instance_exec, :__send__, :__id__, :should_receive, :should_not_receive, :stub, :unstub, :stub_chain, :as_null_object, :null_object?, :received_message?, :should, :should_not]')
+        end).to eq("Me pasaste MiClase y #{MiClase.public_methods}")
 
   end
 end
@@ -55,18 +63,9 @@ end
 
 describe 'Aspect origenes' do
 
-  before(:all) do
-    class MiClase
-
-    end
-    module MiModulo
-
-    end
-  end
-
   it 'chequear parametros pasados' do
     mi_objeto = MiClase.new
-    expect(Aspects.on(MiClase, mi_objeto, MiModulo) { 'hola!' }).to eq("Me pasaste MiClase, #{mi_objeto}, MiModulo y hola!")
+    expect(Aspects.on(MiClase, mi_objeto, MiModulo) { :class }).to eq("Me pasaste MiClase, #{mi_objeto}, MiModulo y class")
   end
 
   it 'falla por no pasarle un bloque' do
@@ -78,15 +77,15 @@ describe 'Aspect origenes' do
   end
 
   it 'acepta regex de una clase que existe' do
-    expect(Aspects.on(/MiClase/) { 'hola' }).to eq ('Me pasaste MiClase y hola')
+    expect(Aspects.on(/MiClase/) { :class }).to eq ('Me pasaste MiClase y class')
   end
 
   it 'falla regex de una clase que no existe' do
-    expect { Aspects.on(/Saraza/) { 'hola' } }.to raise_error(ArgumentError, 'origen vacio')
+    expect { Aspects.on(/Saraza/) { :class } }.to raise_error(ArgumentError, 'origen vacio')
   end
 
   it 'acepta regex parcial de una clase que existe' do
-    expect(Aspects.on(/^Mi.*/) { 'hola' }).to eq ('Me pasaste MiClase, MiModulo y hola')
+    expect(Aspects.on(/^Mi.*/) { :class }).to eq ('Me pasaste MiClase, MiModulo y class')
   end
 end
 
@@ -113,29 +112,10 @@ describe 'Aspect parseo de regex y demaces' do
   end
 
   it 'obtener simbolos de una regex' do
-    expect(Module._get_symbol_by_regex(/Class/)).to eq([:Class, :NilClass, :TrueClass, :FalseClass])
+    expect(Module._get_class_symbol_by_regex(/Class/)).to eq([:Class, :NilClass, :TrueClass, :FalseClass])
   end
 
   it 'obtener ningun simbolo porque no matcheo la regex' do
-    expect(Module._get_symbol_by_regex(/Saraza/)).to eq([])
-  end
-
-
-
-  #Prueba de define_method, no tiene importancia
-  it 'matcheo con nombres de clases' do
-    class MiClase
-
-    end
-    expect(Aspects.new.class_exists?('MiClase')).to eq(true)
-    expect(Aspects.new.class_exists?('MiClase2')).to eq(false)
-  end
-
-  it 'matcheo con nombres de modulos' do
-    module MiModulo
-
-    end
-    expect(Aspects.new.module_exists?('MiModulo')).to eq(true)
-    expect(Aspects.new.module_exists?('MiModulo2')).to eq(false)
+    expect(Module._get_class_symbol_by_regex(/Saraza/)).to eq([])
   end
 end
--- ruby/aspects_ruby/src/aspects.rb
+++ ruby/aspects_ruby/src/aspects.rb
@@ -1,15 +1,14 @@
 class Aspects
   def Aspects.on(*objetos, &condicion)
-    origenes = validar_argumentos(objetos, condicion)
-    condicion.call origenes
-    # Para chequear con los tests
-    "Me pasaste #{origenes.join(', ')} y #{condicion.call origenes}"
+    validar_argumentos(objetos, condicion)
+    @origenes = convertir_a_origenes_validos(objetos)
+    metodos = self.instance_eval &condicion
+    "Me pasaste #{@origenes.join(', ')} y #{metodos}"
   end
 
   def self.validar_argumentos(objetos, condicion)
     raise ArgumentError, 'wrong number of arguments (0 for +1)' if condicion.nil?
     raise ArgumentError, 'origen vacio' if objetos.empty?
-    convertir_a_origenes_validos(objetos)
   end
 
   def self.convertir_a_origenes_validos(objetos)
@@ -17,44 +16,43 @@ class Aspects
     raise ArgumentError, 'origen vacio' if !objetos.get_regexp.empty? && origenes_regex.empty?
     objetos.get_neg_regexp + origenes_regex
   end
+
+
+  def self.where(*condiciones)
+    metodo = condiciones.get_symbols.select { |s| [is_private, is_public].include? s }.first
+    regex_matching = condiciones.select { |s| s.is_a? (Array)}.flatten
+    #El primero que llega se lo queda ;)
+    todos_los_metodos = @origenes.map { |o| o.send(metodo.nil? ? :methods : metodo) }.flatten_lvl_one_unique
+    metodos_filtrados = todos_los_metodos.select {|m| regex_matching.include? m}
+    !metodos_filtrados.empty? ? metodos_filtrados : todos_los_metodos
+  end
+
+  def self.name(regex)
+    Symbol.all_symbols.select { |s| regex.match(s) }
+  end
+
+  def self.is_private
+    :private_methods
+  end
+
+  def self.is_public
+    :public_methods
+  end
+
 end
 
 class Module
-  def _get_symbol_by_regex(regex)
+  def _get_class_symbol_by_regex(regex)
     self.constants.select { |c| regex.match(c) }
   end
 
   def get_origin_by_regex(regex)
-    self._get_symbol_by_regex(regex).map { |symbol| self.const_get(symbol) }
+    self._get_class_symbol_by_regex(regex).map { |symbol| self.const_get(symbol) }
   end
 
   def get_origin_by_multiple_regex(regex)
     regex.map { |r| Module.get_origin_by_regex(r) }.flatten_lvl_one_unique
   end
-end
-
-class Object
-  def where(*condiciones)
-    # TODO: Cambiar a chequeo de condiciones
-    metodo = condiciones.get_symbols.first #El primero que llega se lo queda ;)
-    #Por ahora la ultima condicion es nuestra lista de origenes
-    todos_los_metodos = condiciones.last.map { |c| c.send(metodo.nil? ? :methods : metodo) }.flatten_lvl_one_unique
-    metodos_filtrados = condiciones.get_regexp.map { |r| todos_los_metodos.select { |m| r.match(m) } }.flatten_lvl_one_unique
-    !metodos_filtrados.empty? ? metodos_filtrados : todos_los_metodos
-  end
-
-  def name(regex)
-    regex
-  end
-
-  def is_private
-    :private_methods
-  end
-
-  def is_public
-    :public_methods
-  end
-
 end
 
 class Array
@@ -70,22 +68,8 @@ class Array
     self - get_regexp
   end
 
+
   def flatten_lvl_one_unique
     self.flatten(1).uniq
-  end
-end
-
-
-class Aspects
-
-  #Prueba de define_method, no tiene importancia
-  [Class, Module, Object].each do |origen|
-    define_method("#{origen}_exists?".downcase) do |nombre|
-      begin
-        Module.const_get(nombre).is_a?(origen)
-      rescue NameError
-        false
-      end
-    end
   end
 end
